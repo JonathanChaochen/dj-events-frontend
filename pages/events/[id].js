b0142573b4ba8bfea242643f1a20eff696f4d8bd
Chao Chen <chao.chen@takiwa.co>
2021-04-26 21:20:16 +1200

    Head tags & Layout

--- pages/events/[id].js
+++ pages/events/[id].js
@@ -1,12 +1,12 @@
 import { useRouter } from 'next/router';
+import Layout from '../../components/Layout'
 
 export default function EventPage() {
   const router = useRouter();
-  console.log('router:', router)
   return (
-    <div>
+    <Layout>
       <h1>My Event</h1>
       <h3>{router.query.id}</h3>
-    </div>
+    </Layout>
   );
 }
